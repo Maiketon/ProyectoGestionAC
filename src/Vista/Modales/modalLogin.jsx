146977920b6b13a410ab6ea4e73b82129e736a3e
k0nm1n-n <abrhm2020@gmail.com>
2025-03-15 14:54:17 -0600

    Ajuste a archivos
    Se modifica archivos para validar que en el campo de "archivo pdf" sea unicamente pdf y pese menos de 5mb

--- src/Vista/Modales/modalLogin.jsx
+++ src/Vista/Modales/modalLogin.jsx
@@ -1,5 +1,4 @@
 /*
-
 Autor: Miguel Angel Montoya Bautista
 Fecha: 8-2-25
 Descripcioon: Componente modal de prueba/ejemplo
@@ -7,9 +6,16 @@ Descripcioon: Componente modal de prueba/ejemplo
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
-function ModalLogin({ show, handleClose, mensaje, esIncorrecto }) {
+function ModalLogin({ show, onHide, mensaje, esIncorrecto }) {
   return (
-    <Modal show={show} onHide={handleClose} centered>
+    /* Fecha: 9-3-25
+       Descripción: Ajuste al boton de cerrar modal
+       Se modifica el evento onClick para que cierre el modal pasando de parametro 
+       handleClose
+       al evento
+       onHide
+    */
+    <Modal show={show} onHide={onHide} centered>
       <Modal.Header>
         <Modal.Title className="w-100 text-center">Inicio de Sesión</Modal.Title>
       </Modal.Header>
@@ -18,7 +24,7 @@ function ModalLogin({ show, handleClose, mensaje, esIncorrecto }) {
       </Modal.Body>
       <Modal.Footer className="justify-content-center">
         {esIncorrecto && (
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={onHide}>
             Cerrar
           </Button>
         )}
